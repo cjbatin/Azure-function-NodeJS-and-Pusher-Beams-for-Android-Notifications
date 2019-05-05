54d0929820f4f3078c4a7fbbd2866c8d29afdbe6
Christopher Batin <chris@batin.me>
2019-05-05 20:50:05 +0100

    Init commit

---
+++ HttpTrigger/index.js
@@ -0,0 +1,20 @@
+module.exports = async function (context, req) {
+  const PushNotifications = require('@pusher/push-notifications-server');
+  let beamsClient = new PushNotifications({
+    instanceId: 'YOUR_INSTANCE_ID',
+    secretKey: 'YOUR_SECRET_KEY'
+  });
+  beamsClient.publishToInterests(['hello'], {
+    fcm: {
+            notification: {
+              "title": req.body.title,
+              "body": req.body.message
+            }
+          }
+  }).then((publishResponse) => {
+     context(done);
+  }).catch((error) => {
+    console.log(error);
+    context(done);
+  });
+};
